ae6588ec7d367093cad31322bc9b0c88644b399f
Iv-Umlore <iv.kiselev1998@yandex.ru>
2019-03-08 00:46:07 +0300

    good.

--- BioInformatics/SecondLab.cpp
+++ BioInformatics/SecondLab.cpp
@@ -21,184 +21,79 @@ string TransfortToRNA(string defSTR) {
 char Transform(string Str_3D) {
 
 	if (!Amin(Str_3D[0]) || !Amin(Str_3D[1]) || !Amin(Str_3D[2])) return '0';
-
-	switch (Str_3D[0])
+	// it's autocode, don't worry )
 	{
-	case 'A': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'K';
-			else return 'N';
+			if (Str_3D == "AAA") return 'K';
+			if (Str_3D == "AAC") return 'N';
+			if (Str_3D == "AAG") return 'K';
+			if (Str_3D == "AAU") return 'N';
+			if (Str_3D == "ACA") return 'T';
+			if (Str_3D == "ACC") return 'T';
+			if (Str_3D == "ACG") return 'T';
+			if (Str_3D == "ACU") return 'T';
+			if (Str_3D == "AGA") return 'R';
+			if (Str_3D == "AGC") return 'S';
+			if (Str_3D == "AGG") return 'R';
+			if (Str_3D == "AGU") return 'S';
+			if (Str_3D == "AUA") return 'I';
+			if (Str_3D == "AUC") return 'I';
+			if (Str_3D == "AUG") return 'M';
+			if (Str_3D == "AUU") return 'I';
+			if (Str_3D == "CAA") return 'Q';
+			if (Str_3D == "CAC") return 'H';
+			if (Str_3D == "CAG") return 'Q';
+			if (Str_3D == "CAU") return 'H';
+			if (Str_3D == "CCA") return 'P';
+			if (Str_3D == "CCC") return 'P';
+			if (Str_3D == "CCG") return 'P';
+			if (Str_3D == "CCU") return 'P';
+			if (Str_3D == "CGA") return 'R';
+			if (Str_3D == "CGC") return 'R';
+			if (Str_3D == "CGG") return 'R';
+			if (Str_3D == "CGU") return 'R';
+			if (Str_3D == "CUA") return 'L';
+			if (Str_3D == "CUC") return 'L';
+			if (Str_3D == "CUG") return 'L';
+			if (Str_3D == "CUU") return 'L';
+			if (Str_3D == "GAA") return 'E';
+			if (Str_3D == "GAC") return 'D';
+			if (Str_3D == "GAG") return 'E';
+			if (Str_3D == "GAU") return 'D';
+			if (Str_3D == "GCA") return 'A';
+			if (Str_3D == "GCC") return 'A';
+			if (Str_3D == "GCG") return 'A';
+			if (Str_3D == "GCU") return 'A';
+			if (Str_3D == "GGA") return 'G';
+			if (Str_3D == "GGC") return 'G';
+			if (Str_3D == "GGG") return 'G';
+			if (Str_3D == "GGU") return 'G';
+			if (Str_3D == "GUA") return 'V';
+			if (Str_3D == "GUC") return 'V';
+			if (Str_3D == "GUG") return 'V';
+			if (Str_3D == "GUU") return 'V';
+			if (Str_3D == "UAA") return ' ';
+			if (Str_3D == "UAC") return 'Y';
+			if (Str_3D == "UAG") return ' ';
+			if (Str_3D == "UAU") return 'Y';
+			if (Str_3D == "UCA") return 'S';
+			if (Str_3D == "UCC") return 'S';
+			if (Str_3D == "UCG") return 'S';
+			if (Str_3D == "UCU") return 'S';
+			if (Str_3D == "UGA") return ' ';
+			if (Str_3D == "UGC") return 'C';
+			if (Str_3D == "UGG") return 'W';
+			if (Str_3D == "UGU") return 'C';
+			if (Str_3D == "UUA") return 'L';
+			if (Str_3D == "UUC") return 'F';
+			if (Str_3D == "UUG") return 'L';
+			if (Str_3D == "UUU") return 'F';
 
-		}
-		case 'C': {
-			return 'T';
-			break;
-		}
-		case 'U': {
-			if (Str_3D[2] == 'G') return 'M';
-			else return 'I';
-		}
-		case 'G': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'R';
-			else return 'S';
-		}
-		default:
-			return '0';
-		}
-		break;
-	}
-	case 'C': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			switch (Str_3D[2]) {
-			case 'A': {}
-			case 'G': {
-				return 'Q';
-				break;
-			}
-			default: {
-				return 'H';
-			}
-			}
-			break;
-		}
-		case 'C': {
-			return 'P';
-			break;
-		}
-		case 'U': {
-			return 'L';
-			break;
-		}
-		case 'G': {
-			return 'R';
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	case 'U': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return ' ';
-			else return 'Y';
-			break;
-		}
-		case 'C': {
-			return 'S';
-			break;
-		}
-		case 'U': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'L';
-			else return 'F';
-			break;
-		}
-		case 'G': {
-			switch (Str_3D[2]) {
-			case 'U': {}
-			case'C': {return 'C'; }
-			case 'A': return ' ';
-			case 'G': return 'W';
-			default: return '0';
-			}
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	case 'G': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'E';
-			else return 'D';
-			return '0';
-			break;
-		}
-		case 'C': {
-			if (Amin(Str_3D[2]))
-				return 'A';
-			else return '0';
-			break;
-		}
-		case 'U': {
-			if (Amin(Str_3D[2]))
-				return 'V';
-			else return '0';
-			break;
-		}
-		case 'G': {
-			if (Amin(Str_3D[2]))
-				return 'G';
-			else return '0';
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	default:
-		return '0';
-		break;
-	}
-	return '0';
-}
-
-void Add(char& a) {
-	switch (a) {
-	case 'A': {
-		a = 'C';
-		break;
-	}
-	case 'C': {
-		a = 'G';
-		break;
-	}
-	case 'G': {
-		a = 'U';
-		break;
-	}
-	case 'U': {
-		a = '0';
-		break;
-	}
 	}
 }
 
 int main() {
 
-	ofstream fout;
-	fout.open("AminoToRNA.txt");
-
-	string Str;
-	string str_3d = "";
-	string result = "";	
-
-	for (char a = 'A'; a!='0'; Add(a)) {
-		for (char b = 'A'; b != '0'; Add(b)) {
-			for (char c = 'A'; c != '0'; Add(c)) {
-				str_3d = "";
-				str_3d += a;
-				str_3d += b;
-				str_3d += c;
-				fout << Transform(str_3d) << ' ' << str_3d << endl;
-			}
-		}
-	}
-
-	fout.close();
+	system("pause");
 
 	return 0;
 }
@@ -230,140 +125,76 @@ string TransfortToRNA(string defSTR) {
 char Transform(string Str_3D) {
 
 	if (!Amin(Str_3D[0]) || !Amin(Str_3D[1]) || !Amin(Str_3D[2])) return '0';
-
-	switch (Str_3D[0])
+	// it's autocode, don't worry )
 	{
-	case 'A': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'K';
-			else return 'N';
+			if (Str_3D == "AAA") return 'K';
+			if (Str_3D == "AAC") return 'N';
+			if (Str_3D == "AAG") return 'K';
+			if (Str_3D == "AAU") return 'N';
+			if (Str_3D == "ACA") return 'T';
+			if (Str_3D == "ACC") return 'T';
+			if (Str_3D == "ACG") return 'T';
+			if (Str_3D == "ACU") return 'T';
+			if (Str_3D == "AGA") return 'R';
+			if (Str_3D == "AGC") return 'S';
+			if (Str_3D == "AGG") return 'R';
+			if (Str_3D == "AGU") return 'S';
+			if (Str_3D == "AUA") return 'I';
+			if (Str_3D == "AUC") return 'I';
+			if (Str_3D == "AUG") return 'M';
+			if (Str_3D == "AUU") return 'I';
+			if (Str_3D == "CAA") return 'Q';
+			if (Str_3D == "CAC") return 'H';
+			if (Str_3D == "CAG") return 'Q';
+			if (Str_3D == "CAU") return 'H';
+			if (Str_3D == "CCA") return 'P';
+			if (Str_3D == "CCC") return 'P';
+			if (Str_3D == "CCG") return 'P';
+			if (Str_3D == "CCU") return 'P';
+			if (Str_3D == "CGA") return 'R';
+			if (Str_3D == "CGC") return 'R';
+			if (Str_3D == "CGG") return 'R';
+			if (Str_3D == "CGU") return 'R';
+			if (Str_3D == "CUA") return 'L';
+			if (Str_3D == "CUC") return 'L';
+			if (Str_3D == "CUG") return 'L';
+			if (Str_3D == "CUU") return 'L';
+			if (Str_3D == "GAA") return 'E';
+			if (Str_3D == "GAC") return 'D';
+			if (Str_3D == "GAG") return 'E';
+			if (Str_3D == "GAU") return 'D';
+			if (Str_3D == "GCA") return 'A';
+			if (Str_3D == "GCC") return 'A';
+			if (Str_3D == "GCG") return 'A';
+			if (Str_3D == "GCU") return 'A';
+			if (Str_3D == "GGA") return 'G';
+			if (Str_3D == "GGC") return 'G';
+			if (Str_3D == "GGG") return 'G';
+			if (Str_3D == "GGU") return 'G';
+			if (Str_3D == "GUA") return 'V';
+			if (Str_3D == "GUC") return 'V';
+			if (Str_3D == "GUG") return 'V';
+			if (Str_3D == "GUU") return 'V';
+			if (Str_3D == "UAA") return ' ';
+			if (Str_3D == "UAC") return 'Y';
+			if (Str_3D == "UAG") return ' ';
+			if (Str_3D == "UAU") return 'Y';
+			if (Str_3D == "UCA") return 'S';
+			if (Str_3D == "UCC") return 'S';
+			if (Str_3D == "UCG") return 'S';
+			if (Str_3D == "UCU") return 'S';
+			if (Str_3D == "UGA") return ' ';
+			if (Str_3D == "UGC") return 'C';
+			if (Str_3D == "UGG") return 'W';
+			if (Str_3D == "UGU") return 'C';
+			if (Str_3D == "UUA") return 'L';
+			if (Str_3D == "UUC") return 'F';
+			if (Str_3D == "UUG") return 'L';
+			if (Str_3D == "UUU") return 'F';
 
-		}
-		case 'C': {
-			return 'T';
-			break;
-		}
-		case 'U': {
-			if (Str_3D[2] == 'G') return 'M';
-			else return 'I';
-		}
-		case 'G': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'R';
-			else return 'S';
-		}
-		default:
-			return '0';
-		}
-		break;
 	}
-	case 'C': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			switch (Str_3D[2]) {
-			case 'A': {}
-			case 'G': {
-				return 'Q';
-				break;
-			}
-			default: {
-				return 'H';
-			}
-			}
-			break;
-		}
-		case 'C': {
-			return 'P';
-			break;
-		}
-		case 'U': {
-			return 'L';
-			break;
-		}
-		case 'G': {
-			return 'R';
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	case 'U': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return ' ';
-			else return 'Y';
-			break;
-		}
-		case 'C': {
-			return 'S';
-			break;
-		}
-		case 'U': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'L';
-			else return 'F';
-			break;
-		}
-		case 'G': {
-			switch (Str_3D[2]) {
-			case 'U': {}
-			case'C': {return 'C'; }
-			case 'A': return ' ';
-			case 'G': return 'W';
-			default: return '0';
-			}
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	case 'G': {
-		switch (Str_3D[1])
-		{
-		case 'A': {
-			if (Str_3D[2] == 'A' || Str_3D[2] == 'G') return 'E';
-			else return 'D';
-			return '0';
-			break;
-		}
-		case 'C': {
-			if (Amin(Str_3D[2]))
-				return 'A';
-			else return '0';
-			break;
-		}
-		case 'U': {
-			if (Amin(Str_3D[2]))
-				return 'V';
-			else return '0';
-			break;
-		}
-		case 'G': {
-			if (Amin(Str_3D[2]))
-				return 'G';
-			else return '0';
-			break;
-		}
-		default:
-			return '0';
-			break;
-		}
-		break;
-	}
-	default:
-		return '0';
-		break;
-	}
-	return '0';
 }
+
 
 
 ///////////////////////////////////////////// MAIN ////////////////////////////////
@@ -384,5 +215,13 @@ char Transform(string Str_3D) {
 
 	cout << result;
 
-*/
 
+
+/////////////////////////////////////////////////////////////////////////////////
+
+	ÇÀÄÀÍÈÅ 2
+
+
+/////////////////////////////////////// MAIN ///////////////////////////////////
+
+*/
