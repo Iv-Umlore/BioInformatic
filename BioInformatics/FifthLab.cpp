ec612823131f26b655d0f642c25bac8a96687fa0
Iv-Umlore <iv.kiselev1998@yandex.ru>
2019-04-13 13:53:57 +0300

    5.2 in process

--- BioInformatics/FifthLab.cpp
+++ BioInformatics/FifthLab.cpp
@@ -31,37 +31,92 @@ double GetScore(string str, double** MyArray) {
 	return res;
 }
 
-int main() {
-
-	string OurStr;
-	int ProfileSize;
-	double** Profile;
-
-	getline(cin, OurStr);
-	cin >> ProfileSize;
-
-	Profile = new double*[4];
-
-	for (int i = 0; i < 4; i++) {
-		Profile[i] = new double[ProfileSize];
-		for (int j = 0; j < ProfileSize; j++)
-			cin >> Profile[i][j];
-	}
-
+double GetBestScore(string OurStr, int ProfileSize, double** Profile) {
 	double score = 0.0;
 	double tmp;
-	string MyFavorite = "";
-	string tmpStr = "";
 	for (int SPoint = 0; SPoint < OurStr.length() - ProfileSize + 1; SPoint++) {
-		tmpStr = GetStr(OurStr, SPoint, ProfileSize);			// возможно, что он циклический
-		tmp = GetScore(tmpStr, Profile);		
+		tmp = GetScore(GetStr(OurStr, SPoint, ProfileSize), Profile);
 		if (tmp > score) {
 			score = tmp;
-			MyFavorite = tmpStr;
+		}
+	}
+	return score;
+}
+
+void SetProfile(string str, double** MyProfile, int size) {
+
+}
+
+void ResetProfile(double** MyProfile, int size) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < size; j++) {
+			MyProfile[i][j] = 0.25;
+		}
+	}
+}
+
+void PrintProfile(double** Profile, int size) {
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < size; j++)
+			cout << Profile[i][j] << " ";
+		cout << endl;
+	}
+}
+
+int main() {
+
+	string* OurStr;
+	string str;
+	int ProfileSize;
+	int HMstrings;
+	double** Profile;
+
+	cin >> ProfileSize >> HMstrings;
+	cin.ignore(100, '\n');
+	OurStr = new string[HMstrings];
+
+	for (int i = 0; i < HMstrings; i++)
+		getline(cin, OurStr[i]);
+
+	Profile = new double*[4];
+	for (int i = 0; i < 4; i++) {
+		Profile[i] = new double[ProfileSize];
+		for (int j = 0; j < ProfileSize; j++) {
+			Profile[i][j] = 0.25;
 		}
 	}
 
-	cout << MyFavorite;
+	for (int i = 0; i < OurStr[0].length() - ProfileSize + 1; i++) {
+
+		SetProfile(GetStr(OurStr[0], i, ProfileSize), Profile, ProfileSize);
+
+		for (int j = 0; j < HMstrings; j++) {
+
+			double score = 0.0;
+			double tmp;
+			string MyFavorite = "";
+			string tmpStr = "";
+			for (int SPoint = 0; SPoint < OurStr[j].length() - ProfileSize + 1; SPoint++) {
+				tmpStr = GetStr(OurStr[j], SPoint, ProfileSize);
+				tmp = GetScore(tmpStr, Profile);
+				if (tmp > score) {
+					score = tmp;
+					MyFavorite = tmpStr;
+				}
+			}
+			SetProfile(MyFavorite, Profile, ProfileSize);
+		}
+
+		/*
+		
+		Что-то ещё
+		
+		*/
+
+		ResetProfile(Profile, ProfileSize);
+
+	}
 
 	system("pause");
 	
